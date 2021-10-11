578559d7bf9fbc20c49600424620d4b8814719ec
karthykarthick <karthylock@gmail.com>
2021-10-11 10:41:39 +0530

    message was typed

---
+++ src/democom/demo.java
@@ -0,0 +1,15 @@
+package democom;
+
+public class demo {
+
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hell ooo!");
+
+	}
+
+}
